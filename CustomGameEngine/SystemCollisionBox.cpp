5a63895fe7231a3756e1950f656c7475244bb3cd
henrypaul2001 <henryp1.hp@googlemail.com>
2024-03-19 15:33:31 +0000

    Added some functions to get all possible collision axes of two oriented cubes

--- CustomGameEngine/SystemCollisionBox.cpp
+++ CustomGameEngine/SystemCollisionBox.cpp
@@ -71,4 +71,81 @@ namespace Engine {
 	{
 		return false;
 	}
+
+	std::vector<glm::vec3> SystemCollisionBox::GetCubeNormals(ComponentTransform* transform)
+	{
+		glm::mat3 rotationMatrix = glm::mat3(transform->GetWorldModelMatrix());
+
+		std::vector<glm::vec3> normals;
+		normals.push_back(glm::vec3(1.0f, 0.0f, 0.0f)); // right face
+		normals.push_back(glm::vec3(-1.0f, 0.0f, 0.0f)); // left face
+		normals.push_back(glm::vec3(0.0f, 1.0f, 0.0f)); // top face
+		normals.push_back(glm::vec3(0.0f, -1.0f, 0.0f)); // bottom face
+		normals.push_back(glm::vec3(0.0f, 0.0f, 1.0f)); // front face
+		normals.push_back(glm::vec3(0.0f, 0.0f, -1.0f)); // back face
+
+		// Rotate each normal based on objects current rotation
+		for (glm::vec3 normal : normals) {
+			normal = rotationMatrix * normal;
+		}
+
+		return normals;
+	}
+
+	std::vector<glm::vec3> SystemCollisionBox::GetEdgeVectors(ComponentTransform* transform)
+	{
+		glm::mat3 rotationMatrix = glm::mat3(transform->GetWorldModelMatrix());
+
+		std::vector<glm::vec3> edges;
+		edges.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+		edges.push_back(glm::vec3(-1.0f, 1.0f, 1.0f));
+		edges.push_back(glm::vec3(1.0f, -1.0f, 1.0f));
+		edges.push_back(glm::vec3(-1.0f, -1.0f, 1.0f));
+		edges.push_back(glm::vec3(1.0f, 1.0f, -1.0f));
+		edges.push_back(glm::vec3(-1.0f, 1.0f, -1.0f));
+		edges.push_back(glm::vec3(1.0f, -1.0f, -1.0f));
+		edges.push_back(glm::vec3(-1.0f, -1.0f, -1.0f));
+		edges.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+		edges.push_back(glm::vec3(1.0f, -1.0f, 1.0f));
+		edges.push_back(glm::vec3(-1.0f, 1.0f, 1.0f));
+		edges.push_back(glm::vec3(-1.0f, -1.0f, 1.0f));
+
+		// Rotate each edge by objects current rotation
+		for (glm::vec3 edge : edges) {
+			edge = rotationMatrix * edge;
+		}
+	}
+
+	std::vector<glm::vec3> SystemCollisionBox::GetAllCollisionAxis(ComponentTransform* transform, ComponentCollision* collider, ComponentTransform* transform2, ComponentCollision* collider2)
+	{
+		std::vector<glm::vec3> axes;
+
+		// Get normals of both cubes
+		std::vector<glm::vec3> cube1Normals = GetCubeNormals(transform);
+		std::vector<glm::vec3> cube2Normals = GetCubeNormals(transform2);
+
+		// Get edge vectors of both cubes
+		std::vector<glm::vec3> cube1Edges = GetEdgeVectors(transform);
+		std::vector<glm::vec3> cube2Edges = GetEdgeVectors(transform2);
+
+		// Combine into one list of potential collision axes
+		std::vector<glm::vec3> axes = cube1Normals;
+		axes.insert(axes.end(), cube2Normals.begin(), cube2Normals.end());
+
+		// Add cross product of all edges to axes
+		for (glm::vec3 edge1 : cube1Edges) {
+			for (glm::vec3 edge2 : cube2Edges) {
+				axes.push_back(glm::cross(edge1, edge2));
+			}
+		}
+
+		// Add cross product of all normals to axes
+		for (glm::vec3 normal1 : cube1Normals) {
+			for (glm::vec3 normal2 : cube2Normals) {
+				axes.push_back(glm::cross(normal1, normal2));
+			}
+		}
+
+		return axes;
+	}
 }
--- CustomGameEngine/SystemCollisionBox.h
+++ CustomGameEngine/SystemCollisionBox.h
@@ -9,6 +9,10 @@ namespace Engine {
 		const ComponentTypes MASK = (COMPONENT_TRANSFORM | COMPONENT_COLLISION_BOX);
 
 		bool Intersect(ComponentTransform* transform, ComponentCollision* collider, ComponentTransform* transform2, ComponentCollision* collider2) override;
+
+		std::vector<glm::vec3> GetCubeNormals(ComponentTransform* transform);
+		std::vector<glm::vec3> GetEdgeVectors(ComponentTransform* transform);
+		std::vector<glm::vec3> GetAllCollisionAxis(ComponentTransform* transform, ComponentCollision* collider, ComponentTransform* transform2, ComponentCollision* collider2);
 	public:
 		SystemCollisionBox(EntityManager* entityManager);
 		~SystemCollisionBox();
