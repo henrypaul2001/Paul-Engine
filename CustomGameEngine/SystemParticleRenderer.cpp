a44f50f15af9a5bf425f1b8ef4d0d10692d9fa0d
henrypaul2001 <henryp1.hp@googlemail.com>
2024-06-08 16:56:02 +0100

    Implemented new geometry shader based point particles to particle renderer

--- CustomGameEngine/SystemParticleRenderer.cpp
+++ CustomGameEngine/SystemParticleRenderer.cpp
@@ -43,9 +43,7 @@ namespace Engine {
 
 	void SystemParticleRenderer::Draw(ComponentTransform* transform, ComponentParticleGenerator* generator)
 	{
-		Shader* particleShader = ResourceManager::GetInstance()->DefaultParticleShader();
-
-		particleShader->Use();
+		Shader* particleShader;
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, generator->GetSprite()->id);
@@ -55,17 +53,46 @@ namespace Engine {
 		glDepthMask(GL_FALSE);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE); // additive blending
 
-		particleShader->setBool("sphericalBillboarding", generator->SphericalBillboarding());
+		if (generator->PointSprite()) {
+			glm::vec3 point = glm::vec3(0.0f, 0.0f, 0.0f);
 
-		glm::mat4 model = glm::mat4(1.0f);
-		for (const Particle& particle : generator->GetParticles()) {
-			model = glm::mat4(1.0f);
-			model = glm::translate(model, particle.Position);
-			model = glm::scale(model, particle.Scale);
-			particleShader->setMat4("model", model);
-			particleShader->setVec4("colour", particle.Colour);
-			glBindVertexArray(ResourceManager::GetInstance()->DefaultPlane().VAO);
-			glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(ResourceManager::GetInstance()->DefaultPlane().indices.size()), GL_UNSIGNED_INT, 0);
+			unsigned int VAO, VBO;
+			glGenVertexArrays(1, &VAO);
+			glGenBuffers(1, &VBO);
+			glBindVertexArray(VAO);
+			glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+			glBufferData(GL_ARRAY_BUFFER, 1 * sizeof(glm::vec3), &point, GL_STATIC_DRAW);
+
+			// vertex positions
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
+
+			particleShader = ResourceManager::GetInstance()->DefaultPointParticleShader();
+			particleShader->Use();
+			
+			for (const Particle& particle : generator->GetParticles()) {
+				particleShader->setVec4("colour", particle.Colour);
+				particleShader->setVec2("Scale", glm::vec2(particle.Scale.x, particle.Scale.y));
+				particleShader->setVec3("translation", particle.Position);
+				glDrawArrays(GL_POINTS, 0, 1);
+			}
+		}
+		else {
+			particleShader = ResourceManager::GetInstance()->DefaultParticleShader();
+			particleShader->Use();
+			particleShader->setBool("sphericalBillboarding", generator->SphericalBillboarding());
+
+			glm::mat4 model = glm::mat4(1.0f);
+			for (const Particle& particle : generator->GetParticles()) {
+				model = glm::mat4(1.0f);
+				model = glm::translate(model, particle.Position);
+				model = glm::scale(model, particle.Scale);
+				particleShader->setMat4("model", model);
+				particleShader->setVec4("colour", particle.Colour);
+				glBindVertexArray(ResourceManager::GetInstance()->DefaultPlane().VAO);
+				glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(ResourceManager::GetInstance()->DefaultPlane().indices.size()), GL_UNSIGNED_INT, 0);
+			}
 		}
 
 		glBindVertexArray(0);
