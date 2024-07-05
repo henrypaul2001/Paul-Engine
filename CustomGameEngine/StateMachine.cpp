e6340e0ee27cdb8d0cb9607e0bab7da5e5c615b2
henrypaul2001 <henryp1.hp@googlemail.com>
2024-07-05 13:34:00 +0100

    Set up state machine class

--- CustomGameEngine/StateMachine.cpp
+++ CustomGameEngine/StateMachine.cpp
@@ -2,7 +2,7 @@
 namespace Engine {
 	StateMachine::StateMachine()
 	{
-
+		activeState = nullptr;
 	}
 
 	StateMachine::~StateMachine()
@@ -10,8 +10,33 @@ namespace Engine {
 
 	}
 
+	void StateMachine::AddState(State* newState)
+	{
+		states.emplace_back(newState);
+		if (activeState == nullptr) {
+			activeState = newState;
+		}
+	}
+
+	void StateMachine::AddTransition(StateTransition* newTransition)
+	{
+		statesToTransitions.insert(std::make_pair(newTransition->GetSourceState(), newTransition));
+	}
+
 	void StateMachine::Update()
 	{
+		if (activeState) {
+			// Update state
+			//activeState->Update();
 
+			// Check transition conditions
+			std::pair<TransitionIterator, TransitionIterator> range = statesToTransitions.equal_range(activeState);
+			for (TransitionIterator& i = range.first; i != range.second; i++) {
+				if (i->second->Condition()) {
+					State* newState = i->second->GetDestinationState();
+					activeState = newState;
+				}
+			}
+		}
 	}
 }
--- CustomGameEngine/StateMachine.h
+++ CustomGameEngine/StateMachine.h
@@ -1,19 +1,25 @@
 #pragma once
 #include <map>
+#include "State.h"
+#include <vector>
+#include "StateTransition.h"
 namespace Engine {
+
+	typedef std::multimap<State*, StateTransition*>::iterator TransitionIterator;
+
 	class StateMachine
 	{
 	public:
 		StateMachine();
 		~StateMachine();
 
-		//void AddState(State* newState);
-		//void AddTransition(StateTransition* newTransition);
+		void AddState(State* newState);
+		void AddTransition(StateTransition* newTransition);
 		
 		void Update();
 	private:
-		//State* activeState;
-		//std::vector<State*> states;
-		//std::multimap<State*, StateTransition*> statesToTransitions;
+		State* activeState;
+		std::vector<State*> states;
+		std::multimap<State*, StateTransition*> statesToTransitions;
 	};
 }
