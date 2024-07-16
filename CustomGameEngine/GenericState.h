43593f9f2af99cbd2b1193521d20228f821800c6
henrypaul2001 <henryp1.hp@googlemail.com>
2024-07-16 14:45:01 +0100

    Added enter function and exit function to generic state class

--- CustomGameEngine/GenericState.h
+++ CustomGameEngine/GenericState.h
@@ -6,8 +6,10 @@ namespace Engine {
 	class GenericState : public State
 	{
 	public:
-		GenericState(const std::string& name, StateFunc function, void* data) : State(name) {
-			this->function = function;
+		GenericState(const std::string& name, StateFunc updateFunction, void* data, StateFunc enterFunction = {}, StateFunc exitFunction = {}) : State(name) {
+			this->updateFunction = updateFunction;
+			this->enterFunction = enterFunction;
+			this->exitFunction = exitFunction;
 			this->data = data;
 		}
 		
@@ -15,20 +17,24 @@ namespace Engine {
 		
 		void Update() override {
 			if (data != nullptr) {
-				function(data);
+				updateFunction(data);
 			}
 		}
 
 		void Enter() override {
 			State::Enter();
+			enterFunction(data);
 		}
 
 		void Exit() override {
 			State::Exit();
+			exitFunction(data);
 		}
 
 	protected:
-		StateFunc function;
+		StateFunc updateFunction;
+		StateFunc enterFunction;
+		StateFunc exitFunction;
 		void* data;
 	};
 }
