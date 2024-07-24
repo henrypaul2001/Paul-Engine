3566cfbf423cac00d1641b1be1bf84f6c3abe2cd
henrypaul2001 <henryp1.hp@googlemail.com>
2024-07-24 18:31:25 +0100

    Reflection probe class now generates a cubemap texture

--- CustomGameEngine/ReflectionProbe.cpp
+++ CustomGameEngine/ReflectionProbe.cpp
@@ -1,13 +1,34 @@
 #include "ReflectionProbe.h"
 namespace Engine {
-	ReflectionProbe::ReflectionProbe(unsigned int id, const glm::vec3& position)
+	ReflectionProbe::ReflectionProbe(unsigned int id, const glm::vec3& position, const unsigned int faceResWidth, const unsigned int faceResHeight)
 	{
 		this->fileID = id;
 		this->worldPosition = position;
+		this->faceHeight = faceResHeight;
+		this->faceWidth = faceResWidth;
+
+		SetupCubemapTexture();
 	}
 
 	ReflectionProbe::~ReflectionProbe()
 	{
+		glDeleteTextures(1, &envMap.cubemapID);
+	}
 
+	void ReflectionProbe::SetupCubemapTexture()
+	{
+		glGenTextures(1, &envMap.cubemapID);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, envMap.cubemapID);
+
+		for (unsigned int i = 0; i < 6; i++) {
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, faceWidth, faceHeight, 0, GL_RGB, GL_FLOAT, nullptr);
+		}
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
 	}
 }
--- CustomGameEngine/ReflectionProbe.h
+++ CustomGameEngine/ReflectionProbe.h
@@ -1,14 +1,33 @@
 #pragma once
 #include <glm/ext/vector_float3.hpp>
+#include <glad/glad.h>
 namespace Engine {
+
+	struct ReflectionProbeEnvironmentMap {
+		unsigned int cubemapID;
+		// irradiance id
+		// irradianceID;
+		// prefilterID;
+		// brdf_lutID;
+	};
+
 	class ReflectionProbe
 	{
 	public:
-		ReflectionProbe(const unsigned int id, const glm::vec3& position);
+		ReflectionProbe(const unsigned int id, const glm::vec3& position, const unsigned int faceResWidth = 800, const unsigned int faceResHeight = 800);
 		~ReflectionProbe();
+
+		const ReflectionProbeEnvironmentMap& GetProbeEnvMap() const { return envMap; }
 
 	private:
 		unsigned int fileID;
 		glm::vec3 worldPosition;
+
+		unsigned int faceWidth;
+		unsigned int faceHeight;
+
+		ReflectionProbeEnvironmentMap envMap;
+
+		void SetupCubemapTexture();
 	};
 }
