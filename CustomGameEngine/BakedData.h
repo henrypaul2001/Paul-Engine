3e44bb9e4d69b3b8a7f7c8d410a29e25c900276e
henrypaul2001 <henryp1.hp@googlemail.com>
2024-08-13 15:54:56 +0100

    BakedData class now has the option to initialise a reflection probe individually

--- CustomGameEngine/BakedData.h
+++ CustomGameEngine/BakedData.h
@@ -30,60 +30,15 @@ namespace Engine {
 		void InitialiseReflectionProbes(const std::vector<glm::vec3>& positions, const std::vector<AABBPoints>& localGeometryBounds, const std::vector<float>& soiRadii, const std::vector<unsigned int>& faceRes, const std::vector<float>& nearClips, const std::vector<float>& farClips, const std::vector<bool>& renderSkybox, const std::string& sceneName) {
 			ClearReflectionProbes();
 
-			std::string filePath = "Data/ReflectionProbe/" + sceneName;
-			std::filesystem::path directoryPath = filePath;
-
-			// Create directory if it doesn't already exist
-			if (!std::filesystem::exists(directoryPath)) {
-				std::filesystem::create_directory(directoryPath);
-				std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << filePath << std::endl;
-			}
-
 			for (int i = 0; i < positions.size(); i++) {
-				reflectionProbes.push_back(new ReflectionProbe(i, positions[i], sceneName, localGeometryBounds[i], soiRadii[i], faceRes[i], faceRes[i], nearClips[i], farClips[i], renderSkybox[i]));
-
-				std::string probeString = std::to_string(i);
-
-				// Create file for this reflection probe if it doesn't already exist
-				std::filesystem::path probePath = filePath + "/" + probeString;
-				if (!std::filesystem::exists(probePath)) {
-					std::filesystem::create_directory(probePath);
-					std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << probePath << std::endl;
-				}
-
-				// Create files for each cubemap type
-				// Skybox
-				std::filesystem::path skyboxPath = filePath + "/" + probeString + "/Skybox";
-				if (!std::filesystem::exists(skyboxPath)) {
-					std::filesystem::create_directory(skyboxPath);
-					std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << skyboxPath << std::endl;
-				}
-
-				// Irradiance map
-				std::filesystem::path irradiancePath = filePath + "/" + probeString + "/Irradiance";
-				if (!std::filesystem::exists(irradiancePath)) {
-					std::filesystem::create_directory(irradiancePath);
-					std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << irradiancePath << std::endl;
-				}
-
-				// Prefilter map
-				std::filesystem::path prefilterPath = filePath + "/" + probeString + "/Prefilter";
-				if (!std::filesystem::exists(prefilterPath)) {
-					std::filesystem::create_directory(prefilterPath);
-					std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << prefilterPath << std::endl;
-				}
-
-				// Prefilter mip levels
-				std::string prefilterPathString = prefilterPath.string();
-				std::filesystem::path prefilterMipPath;
-				for (unsigned int mip = 0; mip < 5; mip++) {
-					prefilterMipPath = prefilterPathString + "/Mip " + std::to_string(mip);
-					if (!std::filesystem::exists(prefilterMipPath)) {
-						std::filesystem::create_directory(prefilterMipPath);
-						std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << prefilterMipPath << std::endl;
-					}
-				}
+				InitialiseReflectionProbe(positions[i], localGeometryBounds[i], sceneName, soiRadii[i], faceRes[i], nearClips[i], farClips[i], renderSkybox[i]);
 			}
+		}
+
+		void InitialiseReflectionProbe(const glm::vec3& position, const AABBPoints& localGeometryBounds, const std::string& sceneName, const float sphereOfInfluenceRadius = 30.0f, const unsigned int faceResolution = 512u, const float nearClip = 0.5f, const float farClip = 30.0f, const bool renderSkybox = true) {
+			unsigned int index = reflectionProbes.size();
+			InitialiseProbeDirectory(sceneName, index);
+			reflectionProbes.push_back(new ReflectionProbe(index, position, sceneName, localGeometryBounds, sphereOfInfluenceRadius, faceResolution, faceResolution, nearClip, farClip, renderSkybox));
 		}
 
 		void WriteReflectionProbesToFile();
@@ -103,6 +58,57 @@ namespace Engine {
 				delete probe;
 			}
 			reflectionProbes.clear();
+		}
+		void InitialiseProbeDirectory(const std::string& sceneName, const unsigned int probeIndex) {
+			std::string filePath = "Data/ReflectionProbe/" + sceneName;
+			std::filesystem::path directoryPath = filePath;
+
+			// Create directory if it doesn't already exist
+			if (!std::filesystem::exists(directoryPath)) {
+				std::filesystem::create_directory(directoryPath);
+				std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << filePath << std::endl;
+			}
+			std::string probeString = std::to_string(probeIndex);
+
+			// Create file for this reflection probe if it doesn't already exist
+			std::filesystem::path probePath = filePath + "/" + probeString;
+			if (!std::filesystem::exists(probePath)) {
+				std::filesystem::create_directory(probePath);
+				std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << probePath << std::endl;
+			}
+
+			// Create files for each cubemap type
+			// Skybox
+			std::filesystem::path skyboxPath = filePath + "/" + probeString + "/Skybox";
+			if (!std::filesystem::exists(skyboxPath)) {
+				std::filesystem::create_directory(skyboxPath);
+				std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << skyboxPath << std::endl;
+			}
+
+			// Irradiance map
+			std::filesystem::path irradiancePath = filePath + "/" + probeString + "/Irradiance";
+			if (!std::filesystem::exists(irradiancePath)) {
+				std::filesystem::create_directory(irradiancePath);
+				std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << irradiancePath << std::endl;
+			}
+
+			// Prefilter map
+			std::filesystem::path prefilterPath = filePath + "/" + probeString + "/Prefilter";
+			if (!std::filesystem::exists(prefilterPath)) {
+				std::filesystem::create_directory(prefilterPath);
+				std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << prefilterPath << std::endl;
+			}
+
+			// Prefilter mip levels
+			std::string prefilterPathString = prefilterPath.string();
+			std::filesystem::path prefilterMipPath;
+			for (unsigned int mip = 0; mip < 5; mip++) {
+				prefilterMipPath = prefilterPathString + "/Mip " + std::to_string(mip);
+				if (!std::filesystem::exists(prefilterMipPath)) {
+					std::filesystem::create_directory(prefilterMipPath);
+					std::cout << "BAKEDDATA::REFLECTIONPROBES::Created directory: " << prefilterMipPath << std::endl;
+				}
+			}
 		}
 
 		std::unordered_map<GLenum, std::string> cubemapFaceToString;
