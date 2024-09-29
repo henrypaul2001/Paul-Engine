d95d7def5adff95632dcc145cfc77c8970568425
henrypaul2001 <henryp1.hp@googlemail.com>
2024-09-29 18:05:46 +0100

    Increased floating point size in screen texture buffers

--- CustomGameEngine/RenderManager.cpp
+++ CustomGameEngine/RenderManager.cpp
@@ -559,14 +559,14 @@ namespace Engine {
 		screenTexture = new unsigned int;
 		glGenTextures(1, screenTexture);
 		glBindTexture(GL_TEXTURE_2D, *screenTexture);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, screenWidth, screenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		// alternate
 		alternateScreenTexture = new unsigned int;
 		glGenTextures(1, alternateScreenTexture);
 		glBindTexture(GL_TEXTURE_2D, *alternateScreenTexture);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, screenWidth, screenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
