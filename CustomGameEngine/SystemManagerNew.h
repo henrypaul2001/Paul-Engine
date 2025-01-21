850f02cc038768db133a4d9fd722696ff0ab0740
henrypaul2001 <henryp1.hp@googlemail.com>
2025-01-21 15:16:08 +0000

    Added extra set of systems to SystemManager "PreUpdate" systems

--- CustomGameEngine/SystemManagerNew.h
+++ CustomGameEngine/SystemManagerNew.h
@@ -34,8 +34,35 @@ namespace Engine {
 			}
 		}
 
+		template <typename... Components>
+		bool RegisterPreUpdateSystem(const std::string& systemName, std::function<void(const unsigned int, Components&...)> onActionFunc, std::function<void()> preActionFunc = []() {}, std::function<void()> afterActionFunc = []() {}) {
+			if (preUpdateSystems.find(systemName) != preUpdateSystems.end()) {
+				return false;
+			}
+
+			preUpdateSystems[systemName][0] = preActionFunc;
+			preUpdateSystems[systemName][1] = [this, onActionFunc]() {
+				auto view = ecs->View<Components...>();
+				view.ForEach(onActionFunc);
+				};
+			preUpdateSystems[systemName][2] = afterActionFunc;
+			preUpdateSystemNames.push_back(systemName);
+			return true;
+		}
+
+		void ActionPreUpdateSystems() const {
+			for (const std::string& name : preUpdateSystemNames) {
+				preUpdateSystems.at(name)[0]();
+				preUpdateSystems.at(name)[1]();
+				preUpdateSystems.at(name)[2]();
+			}
+		}
+
 	private:
 		EntityManagerNew* ecs;
+
+		std::vector<std::string> preUpdateSystemNames;
+		std::unordered_map<std::string, std::function<void()>[3]> preUpdateSystems;
 
 		std::vector<std::string> systemNames;
 		std::unordered_map<std::string, std::function<void()>[3]> systems;
