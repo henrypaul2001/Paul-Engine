52d41fb1501f582026a2242e47b75ce40917f98a
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-09 15:23:15 +0000

    Set up Core.h file with declspec macros for different build variations

---
+++ PaulEngine/src/PaulEngine/Core.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifdef PE_PLATFORM_WINDOWS
+	#ifdef PE_BUILD_DLL
+		#define PAUL_API __declspec(dllexport)
+	#else
+		#define PAUL_API __declspec(dllimport)
+	#endif
+#else
+	#error PaulEngine only supports Windows
+#endif
