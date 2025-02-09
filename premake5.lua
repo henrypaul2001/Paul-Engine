f4be82552a1bc5861729013287446b4ba1ca4e55
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-09 17:41:19 +0000

    Set up premake file

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+workspace "PaulEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "PaulEngine"
+	location "PaulEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		buildoptions "/utf-8"
+
+		defines
+		{
+			"PE_PLATFORM_WINDOWS",
+			"PE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "PE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PE_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"PaulEngine/vendor/spdlog/include",
+		"PaulEngine/src"
+	}
+
+	links
+	{
+		"PaulEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		buildoptions "/utf-8"
+
+		defines
+		{
+			"PE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "PE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PE_DIST"
+		optimize "On"
