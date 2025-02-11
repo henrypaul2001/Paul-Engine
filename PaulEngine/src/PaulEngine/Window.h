6dbb23ae9639d9693cf15d061cb240774257c60c
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-11 14:54:24 +0000

    Set up base window class

---
+++ PaulEngine/src/PaulEngine/Window.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "pepch.h"
+#include "Events/Event.h"
+
+namespace PaulEngine {
+	struct WindowProperties {
+		std::string Title;
+		unsigned int Width, Height;
+
+		WindowProperties(const std::string& title = "Paul Engine", unsigned int width = 1280, unsigned int height = 720) : Title(title), Width(width), Height(height) {}
+	};
+
+	// Interface for platform specific windows
+	class PAUL_API Window {
+	public:
+		using EventCallbackFunction = std::function<void(Event&)>;
+		virtual ~Window() = default;
+
+		virtual void OnUpdate() = 0;
+
+		virtual unsigned int GetWidth() const = 0;
+		virtual unsigned int GetHeight() const = 0;
+
+		virtual void SetEventCallback(const EventCallbackFunction& callback) = 0;
+		virtual void SetVSync(bool enabled) = 0;
+		virtual bool IsVSync() const = 0;
+
+		static Window* Create(const WindowProperties& properties = WindowProperties());
+	};
+}
