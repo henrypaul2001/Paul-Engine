c147d7b02cc83e40c673f5f26e9ede9025867668
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-13 14:10:17 +0000

    Fixed postbuild command issue with premake file

--- premake5.lua
+++ premake5.lua
@@ -71,7 +71,7 @@ project "PaulEngine"
 
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 
 	filter "configurations:Debug"
