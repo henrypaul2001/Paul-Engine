0ab787835de608c036bf79ff20f5de7297111c3a
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-14 15:18:44 +0000

    Set up test triangle draw

--- PaulEngine/src/PaulEngine/Application.cpp
+++ PaulEngine/src/PaulEngine/Application.cpp
@@ -1,5 +1,6 @@
 #include "pepch.h"
 #include "Application.h"
+#include <glad/glad.h>
 
 namespace PaulEngine {
 
@@ -15,6 +16,36 @@ namespace PaulEngine {
 
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
+
+		// Temporary opengl test code before moving to abstracted classes
+
+		// Vertex buffer
+		glGenVertexArrays(1, &m_VertexArray);
+		glBindVertexArray(m_VertexArray);
+
+		float vertices[3 * 3] = {
+			-0.5f, -0.5f, 0.0f,
+			0.5, -0.5f, 0.0f,
+			0.0f, 0.5f, 0.0f
+		};
+
+		glGenBuffers(1, &m_VertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+		// Index buffer
+		glGenBuffers(1, &m_IndexBuffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+		unsigned int indices [3] = {
+			0, 1, 2
+		};
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		// Shader
 	}
 
 	Application::~Application()
@@ -38,6 +69,13 @@ namespace PaulEngine {
 	void Application::Run()
 	{
 		while (m_Running) {
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			glViewport(0, 0, m_Window->GetWidth(), m_Window->GetHeight());
+			glBindVertexArray(m_VertexArray);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+
 			// Update layers
 			for (Layer* layer : m_LayerStack) {
 				layer->OnUpdate();
--- PaulEngine/src/PaulEngine/Application.h
+++ PaulEngine/src/PaulEngine/Application.h
@@ -30,6 +30,8 @@ namespace PaulEngine {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+
 		static Application* s_Instance;
 	};
 
