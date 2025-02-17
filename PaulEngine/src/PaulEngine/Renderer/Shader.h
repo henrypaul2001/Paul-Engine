6b92820531d2840eabce6dc813dc6cd83ac665eb
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-17 15:55:27 +0000

    Added UploadMat4 function to shader class

--- PaulEngine/src/PaulEngine/Renderer/Shader.h
+++ PaulEngine/src/PaulEngine/Renderer/Shader.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <glm/glm.hpp>
 
 namespace PaulEngine {
 	class Shader {
@@ -10,6 +11,7 @@ namespace PaulEngine {
 		void Bind() const;
 		void Unbind() const;
 
+		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
 	private:
 		uint32_t m_RendererID;
 	};
