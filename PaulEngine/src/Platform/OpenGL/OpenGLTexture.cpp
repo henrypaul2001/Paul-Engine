947ebc58b86d1204f74016c70104bce8f8fca30e
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-22 14:39:06 +0000

    Added support for file-less texture creation and SetData function

--- PaulEngine/src/Platform/OpenGL/OpenGLTexture.cpp
+++ PaulEngine/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -3,9 +3,22 @@
 
 #include "stb_image.h"
 
-#include <glad/glad.h>
-
 namespace PaulEngine {
+	OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height) : m_Width(width), m_Height(height), m_Path("")
+	{
+		m_InternalFormat = GL_RGBA8;
+		m_DataFormat = GL_RGBA;
+
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+		glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	}
+
 	OpenGLTexture2D::OpenGLTexture2D(const std::string& path) : m_Path(path)
 	{
 		stbi_set_flip_vertically_on_load(true);
@@ -16,34 +29,38 @@ namespace PaulEngine {
 		m_Width = width;
 		m_Height = height;
 
-		GLenum internalFormat = 0;
-		GLenum dataFormat = 0;
+		m_InternalFormat = 0;
+		m_DataFormat = 0;
+
 		if (channels == 4) {
-			internalFormat = GL_RGBA8;
-			dataFormat = GL_RGBA;
+			m_InternalFormat = GL_RGBA8;
+			m_DataFormat = GL_RGBA;
 		}
 		else if (channels == 3) {
-			internalFormat = GL_RGB8;
-			dataFormat = GL_RGB;
+			m_InternalFormat = GL_RGB8;
+			m_DataFormat = GL_RGB;
 		}
 		else if (channels == 2) {
-			internalFormat = GL_RG8;
-			dataFormat = GL_RG;
+			m_InternalFormat = GL_RG8;
+			m_DataFormat = GL_RG;
 		}
 		else if (channels == 1) {
-			internalFormat = GL_RED;
-			dataFormat = GL_RED;
+			m_InternalFormat = GL_RED;
+			m_DataFormat = GL_RED;
 		}
 
-		PE_CORE_ASSERT(internalFormat & dataFormat, "Texture format not supported");
+		PE_CORE_ASSERT(m_InternalFormat & m_DataFormat, "Texture format not supported");
 
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
-		glTextureStorage2D(m_RendererID, 1, internalFormat, m_Width, m_Height);
+		glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
 		
 		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, dataFormat, GL_UNSIGNED_BYTE, data);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
 
 		stbi_image_free(data);
 		stbi_set_flip_vertically_on_load(false);
@@ -52,6 +69,29 @@ namespace PaulEngine {
 	OpenGLTexture2D::~OpenGLTexture2D()
 	{
 		glDeleteTextures(1, &m_RendererID);
+	}
+
+	void OpenGLTexture2D::SetData(void* data, uint32_t size)
+	{
+#ifdef PE_ENABLE_ASSERTS
+		uint32_t sizeofpixel = 0;
+		switch (m_DataFormat) {
+		case GL_RGBA:
+			sizeofpixel = 4;
+			break;
+		case GL_RGB:
+			sizeofpixel = 3;
+			break;
+		case GL_RG:
+			sizeofpixel = 2;
+			break;
+		case GL_RED:
+			sizeofpixel = 1;
+			break;
+		}
+		PE_CORE_ASSERT(size == m_Width * m_Height * sizeofpixel, "Data size must be entire texture!");
+#endif
+		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
 	}
 
 	void OpenGLTexture2D::Bind(const uint32_t slot) const
