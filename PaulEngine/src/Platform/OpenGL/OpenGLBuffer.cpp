2352f19cac48ad1b0891a9cfe606e4244b7d633d
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-24 15:40:41 +0000

    Added SetData function to VertexBuffer

--- PaulEngine/src/Platform/OpenGL/OpenGLBuffer.cpp
+++ PaulEngine/src/Platform/OpenGL/OpenGLBuffer.cpp
@@ -40,6 +40,12 @@ namespace PaulEngine {
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	}
 
+	void OpenGLVertexBuffer::SetData(const void* data, uint32_t size)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, size, data);
+	}
+
 	// --   IndexBuffer   --
 	// ---------------------
 
