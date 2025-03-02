33531cb062325da0dd4162cd0b877e67ca6639de
henrypaul2001 <henryp1.hp@googlemail.com>
2025-03-02 16:11:32 +0000

    Added some default params to camere function

--- PaulEngine/src/PaulEngine/Renderer/Camera.h
+++ PaulEngine/src/PaulEngine/Renderer/Camera.h
@@ -14,12 +14,14 @@ namespace PaulEngine
 		void SetPerspective(const float vfov, const float aspect, const float nearClip = 0.01f, const float farClip = 1000.0f) {
 			m_Projection = glm::perspective(vfov, aspect, nearClip, farClip);
 		}
-		void SetOrthographic(const float left, const float right, const float bottom, const float top, const float nearClip = 0.01f, const float farClip = 1000.0f) {
+		void SetOrthographic(const float left = -1.0f, const float right = 1.0f, const float bottom = -1.0f, const float top = 1.0f, const float nearClip = 0.01f, const float farClip = 1000.0f) {
 			m_Projection = glm::ortho(left, right, bottom, top, nearClip, farClip);
 		}
 
+		// Static functions instead of constructors that look similar due to params all being floats
+
 		static Camera CreatePerspectiveCamera(const float vfov, const float aspect, const float nearClip = 0.01f, const float farClip = 1000.0f) { return Camera(glm::perspective(vfov, aspect, nearClip, farClip)); }
-		static Camera CreateOrthographicCamera(const float left, const float right, const float bottom, const float top, const float nearClip = 0.01f, const float farClip = 1000.0f) { return Camera(glm::ortho(left, right, bottom, top, nearClip, farClip)); }
+		static Camera CreateOrthographicCamera(const float left = -1.0f, const float right = 1.0f, const float bottom = -1.0f, const float top = 1.0f, const float nearClip = 0.01f, const float farClip = 1000.0f) { return Camera(glm::ortho(left, right, bottom, top, nearClip, farClip)); }
 
 	private:
 		glm::mat4 m_Projection;
