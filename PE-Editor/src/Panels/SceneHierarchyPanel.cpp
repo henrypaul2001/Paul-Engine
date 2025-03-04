6fe3d8e4b71b1f901eee46b86926aef70480881a
henrypaul2001 <henryp1.hp@googlemail.com>
2025-03-04 14:59:33 +0000

    Set up basic component property editor in SceneHierarchyPanel class

--- PE-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ PE-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -26,6 +26,12 @@ namespace PaulEngine
 
 			DrawEntityNode(entity);
 		});
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectedEntity) {
+			DrawComponents(m_SelectedEntity);
+		}
 
 		ImGui::End();
 	}
@@ -40,9 +46,42 @@ namespace PaulEngine
 			m_SelectedEntity = entity;
 		}
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered()) {
+			m_SelectedEntity = Entity();
+		}
+
 		if (opened) {
 			ImGui::Text("Hello");
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<ComponentTag>()) {
+			if (ImGui::TreeNodeEx((void*)typeid(ComponentTag).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Tag Component")) {
+				ComponentTag& tagComponent = entity.GetComponent<ComponentTag>();
+
+				char buffer[256];
+				memset(buffer, 0, sizeof(buffer));
+				strcpy_s(buffer, sizeof(buffer), tagComponent.Tag.c_str());
+				if (ImGui::InputText("Tag", buffer, sizeof(buffer))) {
+					tagComponent.Tag = std::string(buffer);
+				}
+
+				ImGui::TreePop();
+			}
+		}
+		
+		if (entity.HasComponent<ComponentTransform>()) {
+			if (ImGui::TreeNodeEx((void*)typeid(ComponentTransform).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform Component")) {
+				ComponentTransform& transform = entity.GetComponent<ComponentTransform>();
+
+				ImGui::DragFloat3("Position", &transform.Position[0], 0.1f);
+				ImGui::DragFloat3("Scale", &transform.Scale[0], 0.1f);
+			
+				ImGui::TreePop();
+			}
 		}
 	}
 }
