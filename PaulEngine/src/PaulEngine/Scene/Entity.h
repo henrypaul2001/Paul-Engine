d48c91c9fb8426fdfcbb287a408e5199d94251df
henrypaul2001 <henryp1.hp@googlemail.com>
2025-03-05 15:27:48 +0000

    Added some conversion operators to entity class

--- PaulEngine/src/PaulEngine/Scene/Entity.h
+++ PaulEngine/src/PaulEngine/Scene/Entity.h
@@ -37,6 +37,9 @@ namespace PaulEngine
 		}
 		operator bool() { return IsValid(); }
 
+		operator entt::entity() const { return m_EntityHandle; }
+		operator uint32_t() const { return (uint32_t)m_EntityHandle; }
+
 		bool operator== (const Entity& other) const { return (m_EntityHandle == other.m_EntityHandle && m_Scene == other.m_Scene); }
 		bool operator!= (const Entity& other) const { return !(*this == other); }
 
