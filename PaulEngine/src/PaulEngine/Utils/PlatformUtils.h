e9307d14da62c858ccf3f10250e91ea680048d26
henrypaul2001 <henryp1.hp@googlemail.com>
2025-03-07 17:46:51 +0000

    Implemented Windows SaveFile dialog

---
+++ PaulEngine/src/PaulEngine/Utils/PlatformUtils.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+namespace PaulEngine
+{
+	class FileDialogs
+	{
+	public:
+		// Returns empty string if cancelled
+		static std::string OpenFile(const char* filter);
+		static std::string SaveFile(const char* filter);
+	};
+}
