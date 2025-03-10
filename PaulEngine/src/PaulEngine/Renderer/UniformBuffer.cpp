e1c0ea1160669905b5db7a5c0d477df841025df0
henrypaul2001 <henryp1.hp@googlemail.com>
2025-03-10 15:14:06 +0000

    Created OpenGL implementation of UniformBuffer class

---
+++ PaulEngine/src/PaulEngine/Renderer/UniformBuffer.cpp
@@ -0,0 +1,23 @@
+#include "pepch.h"
+#include "UniformBuffer.h"
+
+#include "PaulEngine/Renderer/Renderer.h"
+#include "Platform/OpenGL/OpenGLUniformBuffer.h"
+
+namespace PaulEngine
+{
+	Ref<UniformBuffer> UniformBuffer::Create(uint32_t size, uint32_t binding)
+	{
+		PE_PROFILE_FUNCTION();
+		switch (Renderer::GetAPI())
+		{
+			case RenderAPI::API::None:		PE_CORE_ASSERT(false, "RenderAPI::API::None is not supported"); return nullptr;
+			case RenderAPI::API::OpenGL:	return CreateRef<OpenGLUniformBuffer>(size, binding);
+			case RenderAPI::API::Direct3D:  PE_CORE_ASSERT(false, "RenderAPI::API::Direct3D is not supported"); return nullptr;
+			case RenderAPI::API::Vulkan:	PE_CORE_ASSERT(false, "RenderAPI::API::Vulkan is not supported"); return nullptr;
+		}
+
+		PE_CORE_ASSERT(false, "Unknown RenderAPI");
+		return nullptr;
+	}
+}
---
+++ PaulEngine/src/Platform/OpenGL/OpenGLUniformBuffer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "PaulEngine/Renderer/UniformBuffer.h"
+
+namespace PaulEngine
+{
+    class OpenGLUniformBuffer : public UniformBuffer
+    {
+    public:
+        // default usage = GL_DYNAMIC_DRAW
+        OpenGLUniformBuffer(uint32_t size, uint32_t binding, unsigned int usage = 0x88E8);
+        virtual ~OpenGLUniformBuffer();
+
+        virtual void SetData(const void* data, uint32_t size, uint32_t offset = 0) override;
+
+    private:
+        uint32_t m_RendererID;
+
+    };
+}
