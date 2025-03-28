69bc1d579503ee86a275a78894bc1ec82db00c39
henrypaul2001 <henryp1.hp@googlemail.com>
2025-03-28 15:23:06 +0000

    RuntimeAssetManager now temporarily aliases as EditorAssetManager

--- PaulEngine/src/PaulEngine/Asset/RuntimeAssetManager.cpp
+++ PaulEngine/src/PaulEngine/Asset/RuntimeAssetManager.cpp
@@ -3,21 +3,21 @@
 
 namespace PaulEngine
 {
-	Ref<Asset> RuntimeAssetManager::GetAsset(AssetHandle handle)
-	{
-		PE_CORE_ASSERT(false, "Not yet implemented");
-		return Ref<Asset>();
-	}
-
-	bool RuntimeAssetManager::IsAssetRegistered(AssetHandle handle) const
-	{
-		PE_CORE_ASSERT(false, "Not yet implemented");
-		return false;
-	}
-
-	bool RuntimeAssetManager::IsAssetLoaded(AssetHandle handle) const
-	{
-		PE_CORE_ASSERT(false, "Not yet implemented");
-		return false;
-	}
+	//Ref<Asset> RuntimeAssetManager::GetAsset(AssetHandle handle)
+	//{
+	//	PE_CORE_ASSERT(false, "Not yet implemented");
+	//	return Ref<Asset>();
+	//}
+	//
+	//bool RuntimeAssetManager::IsAssetRegistered(AssetHandle handle) const
+	//{
+	//	PE_CORE_ASSERT(false, "Not yet implemented");
+	//	return false;
+	//}
+	//
+	//bool RuntimeAssetManager::IsAssetLoaded(AssetHandle handle) const
+	//{
+	//	PE_CORE_ASSERT(false, "Not yet implemented");
+	//	return false;
+	//}
 }
--- PaulEngine/src/PaulEngine/Asset/RuntimeAssetManager.h
+++ PaulEngine/src/PaulEngine/Asset/RuntimeAssetManager.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "AssetManagerBase.h"
+#include "EditorAssetManager.h"
 
 namespace PaulEngine
 {
-	class RuntimeAssetManager : public AssetManagerBase
-	{
-	public:
-		virtual Ref<Asset> GetAsset(AssetHandle handle) override;
-
-		virtual bool IsAssetRegistered(AssetHandle handle) const override;
-		virtual bool IsAssetLoaded(AssetHandle handle) const override;
-	};
+	// TODO: runtime assets
+	using RuntimeAssetManager = EditorAssetManager;
+	//class RuntimeAssetManager : public AssetManagerBase
+	//{
+	//public:
+	//	virtual Ref<Asset> GetAsset(AssetHandle handle) override;
+	//
+	//	virtual bool IsAssetRegistered(AssetHandle handle) const override;
+	//	virtual bool IsAssetLoaded(AssetHandle handle) const override;
+	//};
 }
