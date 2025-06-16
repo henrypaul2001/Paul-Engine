c5cdfaba7d5e888153075cf1b93b86dc730cde67
henrypaul2001 <henryp1.hp@googlemail.com>
2025-06-16 14:39:09 +0100

    Updated the way asset types are translated to and from strings

--- PaulEngine/src/PaulEngine/Asset/Asset.cpp
+++ PaulEngine/src/PaulEngine/Asset/Asset.cpp
@@ -3,42 +3,59 @@
 
 namespace PaulEngine
 {
+    static const std::unordered_map<std::string, AssetType> s_StringToType = {
+        { "AssetType::None",                    AssetType::None },
+        { "AssetType::Scene",                   AssetType::Scene },
+        { "AssetType::Texture2D",               AssetType::Texture2D },
+        { "AssetType::Font",                    AssetType::Font },
+        { "AssetType::TextureAtlas2D",          AssetType::TextureAtlas2D },
+        { "AssetType::Shader",                  AssetType::Shader },
+        { "AssetType::Material",                AssetType::Material },
+        { "AssetType::Texture2DArray",          AssetType::Texture2DArray },
+        { "AssetType::TextureCubemap",          AssetType::TextureCubemap },
+        { "AssetType::TextureCubemapArray",     AssetType::TextureCubemapArray },
+        { "AssetType::Mesh",                    AssetType::Mesh },
+        { "AssetType::Model",                   AssetType::Model },
+        { "AssetType::Prefab",                  AssetType::Prefab },
+        { "AssetType::EnvironmentMap",          AssetType::EnvironmentMap }
+    };
+
+    static const std::unordered_map<AssetType, std::string> s_TypeToString = {
+        { AssetType::None                   , "AssetType::None"                 },
+        { AssetType::Scene                  , "AssetType::Scene"                },
+        { AssetType::Texture2D              , "AssetType::Texture2D"            },
+        { AssetType::Font                   , "AssetType::Font"                 },
+        { AssetType::TextureAtlas2D         , "AssetType::TextureAtlas2D"       },
+        { AssetType::Shader                 , "AssetType::Shader"               },
+        { AssetType::Material               , "AssetType::Material"             },
+        { AssetType::Texture2DArray         , "AssetType::Texture2DArray"       },
+        { AssetType::TextureCubemap         , "AssetType::TextureCubemap"       },
+        { AssetType::TextureCubemapArray    , "AssetType::TextureCubemapArray"  },
+        { AssetType::Mesh                   , "AssetType::Mesh"                 },
+        { AssetType::Model                  , "AssetType::Model"                },
+        { AssetType::Prefab                 , "AssetType::Prefab"               },
+        { AssetType::EnvironmentMap         , "AssetType::EnvironmentMap"       }
+    };
+
     std::string AssetTypeToString(AssetType type)
     {
-        switch (type)
+        const auto it = s_TypeToString.find(type);
+        if (it != s_TypeToString.end())
         {
-            case AssetType::None: return "AssetType::None";
-            case AssetType::Scene: return "AssetType::Scene";
-            case AssetType::Texture2D: return "AssetType::Texture2D";
-            case AssetType::Font: return "AssetType::Font";
-            case AssetType::TextureAtlas2D: return "AssetType::TextureAtlas2D";
-            case AssetType::Shader: return "AssetType::Shader";
-            case AssetType::Material: return "AssetType::Material";
-            case AssetType::Texture2DArray: return "AssetType::Texture2DArray";
-            case AssetType::TextureCubemap: return "AssetType::TextureCubemap";
-            case AssetType::TextureCubemapArray: return "AssetType::TextureCubemapArray";
-            case AssetType::Mesh: return "AssetType::Mesh";
-            case AssetType::Model: return "AssetType::Model";
-            case AssetType::Prefab: return "AssetType::Prefab";
+            return it->second;
         }
+        PE_CORE_WARN("Undefined asset type translation: {0}", (int)type);
         return "AssetType::!Unknown!";
     }
 
     AssetType AssetTypeFromString(const std::string& assetType)
     {
-        if (assetType == "AssetType::None") { return AssetType::None; }
-        if (assetType == "AssetType::Scene") { return AssetType::Scene; }
-        if (assetType == "AssetType::Texture2D") { return AssetType::Texture2D; }
-        if (assetType == "AssetType::Font") { return AssetType::Font; }
-        if (assetType == "AssetType::TextureAtlas2D") { return AssetType::TextureAtlas2D; }
-        if (assetType == "AssetType::Shader") { return AssetType::Shader; }
-        if (assetType == "AssetType::Material") { return AssetType::Material; }
-        if (assetType == "AssetType::Texture2DArray") { return AssetType::Texture2DArray; }
-        if (assetType == "AssetType::TextureCubemap") { return AssetType::TextureCubemap; }
-        if (assetType == "AssetType::TextureCubemapArray") { return AssetType::TextureCubemapArray; }
-        if (assetType == "AssetType::Mesh") { return AssetType::Mesh; }
-        if (assetType == "AssetType::Model") { return AssetType::Model; }
-        if (assetType == "AssetType::Prefab") { return AssetType::Prefab; }
+        const auto it = s_StringToType.find(assetType);
+        if (it != s_StringToType.end())
+        {
+            return it->second;
+        }
+        PE_CORE_WARN("Undefined asset type translation: {0}", assetType);
         return AssetType::None;
     }
 }
