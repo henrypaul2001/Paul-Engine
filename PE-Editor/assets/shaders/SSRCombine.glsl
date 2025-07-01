fce2b811c37ef68cf7648b3e3fec4856d28d9df1
henrypaul2001 <henryp1.hp@googlemail.com>
2025-07-01 11:44:21 +0100

    Test implementation of brdf in SSR combine pass

--- PE-Editor/assets/shaders/SSRCombine.glsl
+++ PE-Editor/assets/shaders/SSRCombine.glsl
@@ -32,6 +32,11 @@ layout(binding = 1) uniform sampler2D Mat_SSRUVMap;
 
 layout(binding = 2) uniform sampler2D Mat_gWorldPosition;
 layout(binding = 3) uniform sampler2D Mat_gWorldNormal;
+layout(binding = 4) uniform sampler2D Mat_gAlbedo;
+layout(binding = 5) uniform sampler2D Mat_gSpecular;
+layout(binding = 6) uniform sampler2D Mat_gARM;
+layout(binding = 7) uniform sampler2D Mat_gMetadata;
+layout(binding = 8) uniform sampler2D Mat_BRDFLut;
 
 vec4 GetReflectionFromUVMap(vec4 sampleUV)
 {
@@ -55,7 +60,23 @@ void main()
 	vec4 sampleUV = texture(Mat_SSRUVMap, v_TexCoords);
 	vec3 WorldFragPos = texture(Mat_gWorldPosition, v_TexCoords).rgb;
 	vec3 WorldNormal = texture(Mat_gWorldNormal, v_TexCoords).rgb;
-	//vec3 Albedo;
+	vec3 MaterialAlbedo = texture(Mat_gAlbedo, v_TexCoords).rgb;
+
+	vec3 armSample = texture(Mat_gARM, v_TexCoords).rgb;
+	float AO = armSample.r;
+	float Roughness = 0.0;
+	float Metalness = 0.0;
+	int LightingModelIndex = int(texture(Mat_gMetadata, v_TexCoords).g); // 0 = blinn-phong, 1 = pbr
+	if (LightingModelIndex == 0)
+	{
+		float SpecularExponent = texture(Mat_gSpecular, v_TexCoords).a;
+		Roughness = 1.0 - (SpecularExponent / 256.0);
+	}
+	else if (LightingModelIndex == 1)
+	{
+		Roughness = armSample.g;
+		Metalness = armSample.b;
+	}
 
 	vec3 N = WorldNormal;
 	vec3 V = normalize(u_CameraBuffer.ViewPos - WorldFragPos);
@@ -64,17 +85,24 @@ void main()
 	float reflectionAlpha = reflectedColour.a;
 	vec3 currentColour = texture(Mat_LightingPass, v_TexCoords).rgb;
 
-	// Temporary simplified specular logic
-	// TODO: Implement more material detail into these calculations
-	float roughness = 0.0; // all surfaces are perfectly smooth for testing
-	vec3 F0 = vec3(0.0); // all surfaces are full metal for testing
-	vec3 F = FresnelSchlick(max(dot(N, V), 0.0), F0, roughness);
-	vec3 kD = 1.0 - F;
+	vec3 F0 = vec3(0.04);
+	F0 = mix(F0, MaterialAlbedo, Metalness);
 
-	vec3 specular = reflectedColour.rgb * F;
-	vec3 ambience = (kD * specular);
-	
-	currentColour += ambience;
+	float NdotV = max(dot(N, V), 0.0);
+	vec3 F = FresnelSchlick(NdotV, F0, Roughness);
 
-	f_Result = vec4(currentColour, 1.0);
+	vec3 kS = F;
+	vec3 kD = 1.0 - kS;
+	kD *= 1.0 - Metalness;
+
+	vec3 diffuse = MaterialAlbedo;
+
+	vec2 brdf = texture(Mat_BRDFLut, vec2(NdotV, Roughness)).rg;
+	vec3 specular = reflectedColour.rgb * (F * brdf.x + brdf.y);
+
+	vec3 ssrContribution = (kD * diffuse + specular) * AO;
+
+	f_Result = vec4(currentColour + ssrContribution, 1.0);
+	vec3 finalLighting = mix(currentColour, ssrContribution, reflectionAlpha);
+	f_Result = vec4(finalLighting, 1.0);
 }
