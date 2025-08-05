ff0e79175fe0f6c40ced305738c0ec195fb2de3e
henrypaul2001 <henryp1.hp@googlemail.com>
2025-08-05 17:36:45 +0100

    Added RenderPassProfile to render pass

--- PaulEngine/src/PaulEngine/Renderer/RenderPass.h
+++ PaulEngine/src/PaulEngine/Renderer/RenderPass.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "Renderer.h"
+#include "Renderer2D.h"
+
 #include "PaulEngine/Renderer/RenderComponent.h"
 #include "PaulEngine/Scene/Scene.h"
 
@@ -9,6 +12,18 @@ namespace PaulEngine
 	{
 		Ref<Framebuffer> TargetFramebuffer;
 		std::vector<IRenderComponent*> InputComponents;
+	};
+
+	struct RenderPassProfile
+	{
+		uint32_t DrawCalls3D = 0;
+		uint32_t MeshCount = 0;
+		uint32_t UniquePipelines = 0;
+
+		uint32_t DrawCalls2D = 0;
+		uint32_t QuadCount2D = 0;
+		uint32_t CircleCount2D = 0;
+		uint32_t LineCount2D = 0;
 	};
 
 	class RenderPass
@@ -24,13 +39,33 @@ namespace PaulEngine
 		
 		RenderPass(std::vector<RenderComponentType> inputTypes, OnRenderFunc renderFunc) : m_InputTypes(inputTypes), m_RenderFunc(renderFunc) {}
 
-		void OnRender(RenderPassContext context, Ref<Framebuffer> targetFramebuffer, std::vector<IRenderComponent*> inputs) { m_RenderFunc(context, targetFramebuffer, inputs); }
+		void OnRender(RenderPassContext context, Ref<Framebuffer> targetFramebuffer, std::vector<IRenderComponent*> inputs) {
+			m_RenderFunc(context, targetFramebuffer, inputs);
+
+			m_Profile = RenderPassProfile();
+
+			Renderer2D::Statistics stats2D = Renderer2D::GetStats();
+			m_Profile.DrawCalls2D = stats2D.DrawCalls;
+			m_Profile.QuadCount2D = stats2D.QuadCount;
+			m_Profile.CircleCount2D = stats2D.CircleCount;
+			m_Profile.LineCount2D = stats2D.LineCount;
+			Renderer2D::ResetStats();
+
+			Renderer::Statistics stats3D = Renderer::GetStats();
+			m_Profile.DrawCalls3D = stats3D.DrawCalls;
+			m_Profile.MeshCount = stats3D.MeshCount;
+			m_Profile.UniquePipelines = stats3D.PipelineCount;
+			Renderer::ResetStats();
+		}
 
 		const std::vector<RenderComponentType>& GetInputTypes() const { return m_InputTypes; }
 		const UUID& GetRenderID() const { return m_RenderPassID; }
 
+		const RenderPassProfile GetProfile() const { return m_Profile; }
+
 		friend class FrameRenderer;
 	private:
+		RenderPassProfile m_Profile;
 		UUID m_RenderPassID;
 		OnRenderFunc m_RenderFunc;
 	
