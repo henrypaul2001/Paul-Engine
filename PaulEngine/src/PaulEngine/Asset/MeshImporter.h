052738be666cd3923b768bffa8dcaa88017d01a7
henrypaul2001 <henryp1.hp@googlemail.com>
2025-08-26 15:13:32 +0100

    Removed old comments

--- PaulEngine/src/PaulEngine/Asset/MeshImporter.h
+++ PaulEngine/src/PaulEngine/Asset/MeshImporter.h
@@ -27,7 +27,6 @@ namespace PaulEngine
 
 		static void CreatePrefabFromImportedModel(AssetHandle modelHandle);
 
-
 		// Experimental work for batched meshes
 		static Ref<ParsedModelLoadResult> ParseModelFileRaw(const std::filesystem::path& filepath);
 
@@ -36,20 +35,3 @@ namespace PaulEngine
 		static AssetHandle CreateAndImportMeshFile(const std::filesystem::path& baseModelFilepath, const Ref<Mesh>& loadedMesh, uint32_t meshIndex, const AssetHandle modelHandle, const bool persistent);
 	};
 }
-
-// .pmesh file
-// Source: (Model file asset handle)
-// Index: (submesh index)
-
-// On load, read the original model file and retrieve the indexed submesh
-// Performance wise this isn't great. In the case of a large number of submeshes, all submeshes will be parsed by assimp every time one of the meshes is loaded.
-// Instead, maybe a better solution would be to load all meshes once in the event that any one of the meshes are requested
-
-// Import mesh:
-// Ref<Model> baseModel = AssetManager::GetAsset<Model>(.pmesh base model AssetHandle);
-// return baseModel->GetSubmesh(.pmesh submesh index);
-
-// Import model:
-// Ref<Model> loadedModel = LoadModelFile();
-// Create .pmesh files
-// In the future, this would also create a prefab file (when they are supported) that has the correct parent/child relationships for the meshes
